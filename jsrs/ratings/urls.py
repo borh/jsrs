95a27f129677477fe4dbfc7ac6a8a9e283470f44
Bor Hodošček <bor.hodoscek@gmail.com>
2016-03-31 09:30:30 +0900

    whitespace

--- jsrs/ratings/urls.py
+++ jsrs/ratings/urls.py
@@ -6,9 +6,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(
-        regex=r'^$',
-        view=views.ratings_page,
-        name='ratings'
-    ),
+    url(regex=r'^$', view=views.ratings_page, name='ratings'),
 ]
