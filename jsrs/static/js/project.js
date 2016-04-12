d4f40bafc1e146beb9ef1d7ae25bd6962402fb39
Bor Hodošček <bor.hodoscek@gmail.com>
2016-04-12 09:43:41 +0900

    do not run js if play button is not found

--- jsrs/static/js/project.js
+++ jsrs/static/js/project.js
@@ -6,28 +6,30 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  document.getElementById('play-button').onclick = function() {
-    if (document.getElementById('play-button').classList.contains('active')) {
-      return;
-    }
-    document.getElementById('a').play();
-    // console.log(document.getElementById('a').currentTime);
-    // document.getElementById('a').addEventlistener('loadedmetadata', function() {
-    //   console.log(document.getElementById('a').duration);
-    // });
+  if (document.getElementById('play-button')) {
+    document.getElementById('play-button').onclick = function() {
+      if (document.getElementById('play-button').classList.contains('active')) {
+        return;
+      }
+      document.getElementById('a').play();
+      // console.log(document.getElementById('a').currentTime);
+      // document.getElementById('a').addEventlistener('loadedmetadata', function() {
+      //   console.log(document.getElementById('a').duration);
+      // });
 
-    document.getElementById('play-button').classList.toggle('active');
-    document.getElementById('play-button').classList.toggle('disabled');
-    document.getElementById('play-button').innerHTML = 'Ａを再生中';
-  };
+      document.getElementById('play-button').classList.toggle('active');
+      document.getElementById('play-button').classList.toggle('disabled');
+      document.getElementById('play-button').innerHTML = 'Ａを再生中';
+    };
 
-  document.getElementById('a').onended = function(e) {
-    document.getElementById('b').play();
-    document.getElementById('play-button').innerHTML = 'Ｂを再生中';
-  };
+    document.getElementById('a').onended = function(e) {
+      document.getElementById('b').play();
+      document.getElementById('play-button').innerHTML = 'Ｂを再生中';
+    };
 
-  document.getElementById('b').onended = function(e) {
-    document.getElementById('play-button').classList.toggle('hide');
-    document.getElementById('rating').classList.toggle('hide');
-  };
+    document.getElementById('b').onended = function(e) {
+      document.getElementById('play-button').classList.toggle('hide');
+      document.getElementById('rating').classList.toggle('hide');
+    };
+  }
 });
