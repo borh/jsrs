3eeca7023f6a635424fcbece55d207489ec8b443
Bor Hodošček <bor.hodoscek@gmail.com>
2016-05-02 19:11:35 +0900

    added app prefix to admin site

--- config/settings/common.py
+++ config/settings/common.py
@@ -230,6 +230,6 @@ AUTOSLUG_SLUGIFY_FUNCTION = 'slugify.slugify'
 
 
 # Location of root django.contrib.admin URL, use {% url 'admin:index' %}
-ADMIN_URL = r'^admin/'
+ADMIN_URL = r'^app/admin/'
 
 # Your common stuff: Below this line define 3rd party library settings
