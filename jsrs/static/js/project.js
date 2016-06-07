a67364830b58b3a2a48ef4a25c2228f52fa4daac
Bor Hodošček <bor.hodoscek@gmail.com>
2016-06-07 15:13:09 +0900

    tweaked browser-detect

--- jsrs/static/js/project.js
+++ jsrs/static/js/project.js
@@ -10,7 +10,7 @@
 var browserDetect = function() {
   var ua = navigator.userAgent;
   var detected;
-  if (/Mobile|mobile/i.test(ua) && /Chrome|iPad|iPod|iPhone/i.test(ua))
+  if (/Mobile|mobile/i.test(ua) && /Chrome|iPad|iPod|iPhone/i.test(ua) && !/Safari/i.test(ua))
     detected = 'mobile chrome';
   else if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|mobile|CriOS/i.test(ua))
     detected = 'mobile';
@@ -34,52 +34,71 @@ var switcharoo = function(e, dummyUrl) {
   }
 }
 
+var play_audio = function(ua, dummy_url, switched) {
+  if (!switched && /mobile/i.test(ua)) {
+    // Workaround for broken audio in mobile Chrome (https://bugs.chromium.org/p/chromium/issues/detail?id=178297)
+    if (document.getElementById('dummy')) {
+      switcharoo(document.getElementById('a'), dummy_url);
+      //switcharoo(document.getElementById('b'), dummy_url);
+    }
+  }
+
+  document.getElementById('play-button').onclick = function() {
+    if (document.getElementById('play-button').classList.contains('active')) {
+      return;
+    }
+    document.getElementById('a').play();
+    // console.log(document.getElementById('a').currentTime);
+    // document.getElementById('a').addEventlistener('loadedmetadata', function() {
+    //   console.log(document.getElementById('a').duration);
+    // });
+
+    document.getElementById('play-button').classList.toggle('active');
+    document.getElementById('play-button').classList.toggle('disabled');
+    document.getElementById('play-button').disabled = true;
+    document.getElementById('play-button').innerHTML = 'Ａを再生中';
+  };
+
+  document.getElementById('a').onended = function(e) {
+    if (!switched && /mobile/i.test(ua))
+      switcharoo(document.getElementById('b'), dummy_url);
+
+    document.getElementById('b').play();
+    document.getElementById('play-button').innerHTML = 'Ｂを再生中';
+  };
+
+  document.getElementById('b').onended = function(e) {
+    document.getElementById('play-button').classList.toggle('hide');
+    document.getElementById('rating').classList.toggle('hide');
+  };
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
 
   var ua = browserDetect();
-  if (ua === 'mobile chrome') {
-    // Workaround for broken audio in mobile Chrome (https://bugs.chromium.org/p/chromium/issues/detail?id=178297)
-    if (document.getElementById('dummy')) {
-      var dummy_url = document.getElementById('dummy').src;
-      switcharoo(document.getElementById('a'), dummy_url);
-    }
-  // End of workaround
-  }
+  var dummy_url = document.getElementById('dummy').src;
+  var switched = false;
 
   if (document.getElementById('play-button')) {
-    document.getElementById('play-button').onclick = function() {
-      if (document.getElementById('play-button').classList.contains('active')) {
-        return;
-      }
-      document.getElementById('a').play();
-      if (ua === 'mobile chrome')
-        switcharoo(document.getElementById('b'), dummy_url);
-      // console.log(document.getElementById('a').currentTime);
-      // document.getElementById('a').addEventlistener('loadedmetadata', function() {
-      //   console.log(document.getElementById('a').duration);
-      // });
+    play_audio(ua, dummy_url, switched);
 
-      document.getElementById('play-button').classList.toggle('active');
-      document.getElementById('play-button').classList.toggle('disabled');
-      document.getElementById('play-button').disabled = true;
-      document.getElementById('play-button').innerHTML = 'Ａを再生中';
-    };
+  //window.setTimeout(function() {
+  // NOTE: Cannot autoplay on mobile.
+  if (document.querySelector('.autoplay') && !/mobile/i.test(ua)) {
+    console.log('Autoplaying audio...');
+    document.getElementById('play-button').click();
+  } else if (document.querySelector('.autoplay') && /mobile/i.test(ua)) {
+    console.log('Autoplaying audio on mobile using touchend...');
+    addEventListener('touchend', function (e) {
+      console.log('.');
+      play_audio(ua, dummy_url, switched);
+      //soundHandle.src = 'audio.mp3';
+      //soundHandle.loop = true;
+      //soundHandle.play();
+      //soundHandle.pause();
+    });
+  }
 
-    document.getElementById('a').onended = function(e) {
-      document.getElementById('b').play();
-      document.getElementById('play-button').innerHTML = 'Ｂを再生中';
-    };
-
-    document.getElementById('b').onended = function(e) {
-      document.getElementById('play-button').classList.toggle('hide');
-      document.getElementById('rating').classList.toggle('hide');
-    };
-
-    //window.setTimeout(function() {
-    if (document.querySelector('.autoplay')) {
-      console.log('Autoplaying audio...');
-      document.getElementById('play-button').click();
-    }
-    //}, 200);
+  //}, 200);
   }
 });
