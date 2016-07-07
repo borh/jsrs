5d308996c15d07a1c1aa07541dd44cf16f57fd86
Bor Hodošček <bor.hodoscek@gmail.com>
2016-07-07 22:32:01 +0900

    fixed faulty form logic
    Previously we would re-instatiate the forms audio files after the rating
    was already sent, invalidating the results under indeterministic
    conditions. Audio file selection was changed to be more random in a
    previous changset, which broke the previous 'correct' behavior (only
    correct by change--i.e. under single rater conditions, and only before
    the randomization changeset).

--- jsrs/ratings/forms.py
+++ jsrs/ratings/forms.py
@@ -4,11 +4,17 @@ from django.forms import ModelForm
 from .models import Ratings, BOOL_CHOICES
 
 class RatingsForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(RatingsForm, self).__init__(*args, **kwargs)
+
+
     class Meta:
         model = Ratings
-        fields = ('a_gt_b',)
+        fields = ('a_gt_b', 'audio_a', 'audio_b')
         widgets = {
             'a_gt_b': forms.RadioSelect(choices=BOOL_CHOICES),
+            'audio_a': forms.HiddenInput(),
+            'audio_b': forms.HiddenInput()
         }
 
 # class RatingForm(forms.Form):
--- jsrs/ratings/views.py
+++ jsrs/ratings/views.py
@@ -23,25 +23,37 @@ def ratings_page(request):
         return HttpResponseRedirect(reverse('users:rater_survey'))
     #if not request.user.is_authenticated(): return render(request, 'ratings/ratings.html')
 
-    sound_file_a, sound_file_b, sentence_text, (mdpref_results, mdpref_svg) = get_next_rating(request.user.id)
+    # print('ratings_page entered with {}'.format(request.method))
+    # sound_file_a, sound_file_b, sentence_text, (mdpref_results, mdpref_svg) = get_next_rating(request.user.id)
 
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = RatingsForm(request.POST)
+        # print('F:{}, valid?:{}'.format([sound_file_a, sound_file_b], form.is_valid()))
         # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
-            r = Ratings(audio_a=sound_file_a, audio_b=sound_file_b, user=request.user, a_gt_b=form.cleaned_data['a_gt_b'])
+            # print('R:{}, choice:{}\nF:{}'.format([form.cleaned_data['audio_a'], form.cleaned_data['audio_b']], form['a_gt_b'], form))
+            r = Ratings(audio_a=form.cleaned_data['audio_a'], audio_b=form.cleaned_data['audio_b'], user=request.user, a_gt_b=form.cleaned_data['a_gt_b'])
             if request.user.is_superuser == False:
                 r.save()
             return HttpResponseRedirect(reverse('ratings:ratings'))
+        # print('form invalid')
+        return HttpResponseRedirect(reverse('ratings:ratings'))
 
-        # if a GET (or any other method) we'll create a blank form
-    else:
-        form = RatingsForm()
+    # if a GET (or any other method) we'll create a blank form
+
+    sound_file_a, sound_file_b, sentence_text, (mdpref_results, mdpref_svg) = get_next_rating(request.user.id)
+
+    form = RatingsForm(
+        data={'audio_a': sound_file_a,
+              'audio_b': sound_file_b},
+        initial={'audio_a': sound_file_a,
+                 'audio_b': sound_file_b}
+    )
 
     rated = ratings_done(request.user.id)
     rated_goal = 50 # TODO make ratings_set-specific
