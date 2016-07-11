31c382b841a48a93e50dc63194e3d72aaa3a12e9
Bor Hodošček <bor.hodoscek@gmail.com>
2016-07-11 13:32:28 +0900

    added timestamp to ratings model

--- jsrs/ratings/models.py
+++ jsrs/ratings/models.py
@@ -19,6 +19,7 @@ class Ratings(models.Model):
     audio_b = models.ForeignKey(Audio, related_name='audio_b_fk', on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     a_gt_b = models.BooleanField(verbose_name='AがBより良い', choices=BOOL_CHOICES, db_index=True) # True/1 -> a is better; False/0 -> b is better
+    timestamp = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name_plural = 'Ratings'
