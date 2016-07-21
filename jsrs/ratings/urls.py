0d005893ac26ea1a0970bd6f944867afd7b6ed82
Bor Hodošček <bor.hodoscek@gmail.com>
2016-07-21 13:20:52 +0900

    added urls for new views

--- jsrs/ratings/urls.py
+++ jsrs/ratings/urls.py
@@ -6,4 +6,7 @@ from . import views
 urlpatterns = [
     url(regex=r'^$', view=views.ratings_page, name='ratings'),
     url(regex=r'^ratings_done$', view=views.get_ratings_done, name='ratings_done'),
+    url(regex=r'^export_table', view=views.ratings_export_table, name='ratings_export_table'),
+    url(regex=r'^next_pair', view=views.next_pair_export_table, name='next_pair_export_table'),
+    url(regex=r'^mdpref', view=views.mdpref_results, name='ratings_mdpref_results'),
 ]
