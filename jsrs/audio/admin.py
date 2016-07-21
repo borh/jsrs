d5aa899969f02bf4816cd70fbd1191dfe66baf58
Bor Hodošček <bor.hodoscek@gmail.com>
2016-07-21 12:43:59 +0900

    made reader and sentence models visible from admin

--- jsrs/audio/admin.py
+++ jsrs/audio/admin.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 
-from .models import Audio
+from .models import Audio, Reader, Sentence
 
 admin.site.register(Audio)
+admin.site.register(Reader)
+admin.site.register(Sentence)
