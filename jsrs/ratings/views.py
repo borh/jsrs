3660ba57b69490516df1f0411dc721ba7675c9b4
Bor Hodošček <bor.hodoscek@gmail.com>
2016-07-21 13:22:05 +0900

    improved logging in main ratings function & misc
    Factored out mdpref calculation into new view. Logging still needs more testing.

--- jsrs/ratings/views.py
+++ jsrs/ratings/views.py
@@ -27,32 +27,45 @@ def ratings_page(request):
         rater = Rater.objects.get(user_id=request.user.id)
     except Rater.DoesNotExist:
         return HttpResponseRedirect(reverse('users:rater_survey'))
-    #if not request.user.is_authenticated(): return render(request, 'ratings/ratings.html')
-
-    # print('ratings_page entered with {}'.format(request.method))
-    # sound_file_a, sound_file_b, sentence_text, (mdpref_results, mdpref_svg) = get_next_rating(request.user.id)
 
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = RatingsForm(request.POST)
-        # print('F:{}, valid?:{}'.format([sound_file_a, sound_file_b], form.is_valid()))
+
         # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            # print('R:{}, choice:{}\nF:{}'.format([form.cleaned_data['audio_a'], form.cleaned_data['audio_b']], form['a_gt_b'], form))
-            r = Ratings(audio_a=form.cleaned_data['audio_a'], audio_b=form.cleaned_data['audio_b'], user=request.user, a_gt_b=form.cleaned_data['a_gt_b'])
+            audio_a = form.cleaned_data['audio_a']
+            audio_b = form.cleaned_data['audio_b']
+
+            r = Ratings(
+                audio_a=audio_a,
+                audio_b=audio_b,
+                reader_a=audio_a.reader,
+                reader_b=audio_b.reader,
+                sentence=audio_a.sentence,
+                a_gt_b=form.cleaned_data['a_gt_b'],
+                user=request.user
+            )
+            logger.debug(
+                'Rating POST requested for user={} sound_file_a={} sound_file_b={} a_gt_b={}'.format(
+                    request.user.id,
+                    form.cleaned_data['audio_a'],
+                    form.cleaned_data['audio_b'],
+                    form.cleaned_data['a_gt_b']
+                )
+            )
             if request.user.is_superuser == False:
                 r.save()
+            # Redirect back to ratings page for new rating:
             return HttpResponseRedirect(reverse('ratings:ratings'))
-        # print('form invalid')
+        logger.warning('Rating POST invalid for user={} form={}'.format(request.user.id, form))
         return HttpResponseRedirect(reverse('ratings:ratings'))
 
     # if a GET (or any other method) we'll create a blank form
 
-    sound_file_a, sound_file_b, sentence_text, (mdpref_results, mdpref_svg) = get_next_rating(request.user.id)
+    sound_file_a, sound_file_b, sentence_text = get_next_rating(request.user.id)
 
     form = RatingsForm(
         data={'audio_a': sound_file_a,
@@ -62,18 +75,14 @@ def ratings_page(request):
     )
 
     rated = ratings_done(request.user.id)
-    rated_goal = 50 # TODO make ratings_set-specific
+    rated_goal = 50 # TODO make sentence set-specific
 
-    # Do not send mdprefmx results for non-admin users.
-    if request.user.is_superuser == False:
-        mdpref_results = None
+    logger.debug('New rating requested for user={} sound_file_a={} sound_file_b={}'.format(request.user.id, sound_file_a.id, sound_file_b.id))
 
     return render(request,
                   'ratings/ratings.html',
                   {'form': form,
                    'user_id': request.user.id,
-                   'mdpref_results': mdpref_results,
-                   'mdpref_svg': mdpref_svg,
                    'rated': rated,
                    'rated_progress': round(rated / rated_goal * 100),
                    'rated_goal': rated_goal,
@@ -81,7 +90,16 @@ def ratings_page(request):
                    'sound_file_a': sound_file_a.path,
                    'sound_file_b': sound_file_b.path})
 
-from django.http import JsonResponse
+
+@login_required
+def mdpref_results(request):
+    data = [get_mdpref_results(sentence_id) for sentence_id in range(1,41)]
+    return render(request,
+                  'ratings/mdpref.html',
+                  {'data': data})
+
+
+@login_required
 def get_ratings_done(request):
     return JsonResponse({'ratings_done': ratings_done(request.user.id)})
 
@@ -89,19 +107,33 @@ def get_ratings_done(request):
 @login_required
 def ratings_export_table(request):
     data = get_all_ratings_summary()
-    sio = StringIO()
-    PandasDataFrame = pd.DataFrame(data)
-    PandasWriter = pd.ExcelWriter(sio, engine='xlsxwriter')
-    PandasDataFrame.to_excel(PandasWriter, sheet_name=sheetname)
-    PandasWriter.save()
+    df = read_frame(data)
+    return HttpResponse(df.to_html())
 
-    sio.seek(0)
-    workbook = sio.getvalue()
 
-    h = hashlib.new('ripemd160')
-    h.update(data)
-    filename = 'jsrs-ratings-data-{}-{}.xlsx'.format(datetime.datetime.now().strftime('%Y-%m-%d_%H%I'), h.hexdigest())
+@login_required
+def next_pair_export_table(request):
+    data = get_unrated_pair(request.user.id)
+    df = pd.DataFrame.from_records(data)
+    return HttpResponse(df.to_html())
 
-    response = StreamingHttpResponse(workbook, content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-    response['Content-Disposition'] = 'attachment; filename=%s' % filename
-    return response
+# from io import StringIO
+# import datetime
+# import hashlib
+
+    # FIXME Pandoc's Excel export errors out in timestampz conversion (both engines).
+    #sio = StringIO()
+    # PandasWriter = pd.ExcelWriter(sio, engine='openpyxl') #engine='xlsxwriter')
+    # PandasDataFrame.to_excel(PandasWriter, sheet_name='ratings_ratings')
+    # PandasWriter.save()
+    #
+    # sio.seek(0)
+    # workbook = sio.getvalue()
+    #
+    # h = hashlib.new('ripemd160')
+    # h.update(data)
+    # filename = 'jsrs-ratings-data-{}-{}.xlsx'.format(datetime.datetime.now().strftime('%Y-%m-%d_%H%I'), h.hexdigest())
+    #
+    # response = StreamingHttpResponse(workbook, content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    # response['Content-Disposition'] = 'attachment; filename=%s' % filename
+    # return response
