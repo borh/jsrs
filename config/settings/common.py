e90a80a6e5b654bba0b15a9b809ba48fe7177b33
Bor Hodošček <bor.hodoscek@gmail.com>
2016-07-23 20:18:41 +0900

    redirection fixes for login and logout

--- config/settings/common.py
+++ config/settings/common.py
@@ -224,7 +224,8 @@ SOCIALACCOUNT_ADAPTER = 'jsrs.users.adapters.SocialAccountAdapter'
 # Custom user app defaults
 # Select the correct user model
 AUTH_USER_MODEL = 'users.User'
-LOGIN_REDIRECT_URL = 'users:redirect'
+LOGIN_REDIRECT_URL = 'ratings:ratings'
+LOGOUT_REDIRECT_URL = 'home'
 LOGIN_URL = 'account_login'
 
 # SLUGLIFIER
