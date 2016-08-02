8fbe0ccea8b69ace7c7e5cc3328e2477ad5e7fbb
Bor Hodošček <bor.hodoscek@gmail.com>
2016-08-02 17:49:28 +0900

    fixed default url

--- config/settings/common.py
+++ config/settings/common.py
@@ -177,7 +177,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 STATIC_ROOT = str(ROOT_DIR('staticfiles'))
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
-STATIC_URL = '/static/'
+STATIC_URL = '/app/static/'
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = (
